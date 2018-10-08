aa85b469cd059957c4024936a0b253d5b44971f8
PinkHonkKongGmr <39237301+PinkHonkKongGmr@users.noreply.github.com>
2018-10-08 17:33:34 +0300

    зарождается интерфейс
    по клику между двумя станциями выдается кратчайшее расстояние
    пока срабатывает только в первый раз, так как не пересчитывает заново и учитываются все минималки

--- js/script.js
+++ js/script.js
@@ -15,8 +15,8 @@ class point {
   }
 }
 
-var baltiyskay = new point('baltiyskaya', 1000, 1, 2, 3);
-stations.push(baltiyskay);
+var baltiyskaya = new point('baltiyskaya', 1000, 1, 2, 3);
+stations.push(baltiyskaya);
 var depo = new point('depo', 1000, 1, 2, 3);
 stations.push(depo);
 var leninskiy = new point('leninskiy', 1000, 1, 2, 3);
@@ -43,41 +43,40 @@ var grustnaya_zhizn = new point('grustnaya_zhizn', 1000, 3);
 stations.push(grustnaya_zhizn);
 
 class way {
-  constructor(a, b, dist, cost, width, ...ind) {
+  constructor(a, b, dist, cost, ...ind) {
     this.point_a = a.station;
     this.point_b = b.station;
     this.distation = dist;
     this.price = cost;
-    this.width = width;
     this.index = ind;
   }
 }
 
-var balt_depo = new way(baltiyskay, depo, 3, 10, 1000, 1, 2, 3);
+var balt_depo = new way(baltiyskaya, depo, 3, 10, 1, 2, 3);
 railways.push(balt_depo);
-var depo_len = new way(depo, leninskiy, 5, 12, 1000, 1, 2, 3);
+var depo_len = new way(depo, leninskiy, 5, 12, 1, 2, 3);
 railways.push(depo_len);
-var len_taycy = new way(leninskiy, taycy, 15, 30, 1000, 1);
+var len_taycy = new way(leninskiy, taycy, 15, 30, 1);
 railways.push(len_taycy);
-var taycy_gatchina = new way(taycy, gatchina, 7, 14, 1000, 1);
+var taycy_gatchina = new way(taycy, gatchina, 7, 14, 1);
 railways.push(taycy_gatchina);
-var gatchina_myshkino = new way(gatchina, myshkino, 10, 22, 1000, 1);
+var gatchina_myshkino = new way(gatchina, myshkino, 10, 22, 1);
 railways.push(gatchina_myshkino);
-var myshkino_gorgorod = new way(myshkino, gorgorod, 12, 25, 1000, 1, 2);
+var myshkino_gorgorod = new way(myshkino, gorgorod, 12, 25, 1, 2);
 railways.push(myshkino_gorgorod);
-var len_strelna = new way(leninskiy, strelna, 11, 20, 1000, 2);
+var len_strelna = new way(leninskiy, strelna, 11, 20, 2);
 railways.push(len_strelna);
-var str_petergof = new way(strelna, petergof, 8, 15, 1000, 2);
+var str_petergof = new way(strelna, petergof, 8, 15, 2);
 railways.push(str_petergof);
-var pet_lomonosov = new way(petergof, lomonosov, 9, 16, 1000, 2);
+var pet_lomonosov = new way(petergof, lomonosov, 9, 16, 2);
 railways.push(pet_lomonosov);
-var lom_popovo = new way(lomonosov, popovo, 7, 18, 1000, 2);
+var lom_popovo = new way(lomonosov, popovo, 7, 18, 2);
 railways.push(lom_popovo);
-var popovo_gorgorod = new way(popovo, gorgorod, 2, 5, 1000, 1, 2);
+var popovo_gorgorod = new way(popovo, gorgorod, 2, 5, 1, 2);
 railways.push(popovo_gorgorod);
-var len_ves = new way(leninskiy, vesyolaya_zhizn, 35, 50, 1000, 3);
+var len_ves = new way(leninskiy, vesyolaya_zhizn, 35, 50, 3);
 railways.push(len_ves);
-var ves_gr = new way(vesyolaya_zhizn, grustnaya_zhizn, 3, 5, 1000, 3);
+var ves_gr = new way(vesyolaya_zhizn, grustnaya_zhizn, 3, 5, 3);
 railways.push(ves_gr);
 
 function theStation(nodes, name) {
@@ -88,24 +87,8 @@ function theStation(nodes, name) {
   }
 }
 
-function copymaker(arr) {
-  let copy = arr.map(function(a) {
-    return a;
-  });
-  return copy;
-}
 
-function fireStarter(start) {
-  let num;
-  for (let i in stations) {
-    if (stations[i] == start) {
-      num = i;
-    }
-  }
-  return num;
-}
-
-function widthCompare(obj, comp, width) {
+function weightCompare(obj, comp, width) {
 
   if (obj.weight > comp + width) {
     obj.weight = comp + width
@@ -113,13 +96,13 @@ function widthCompare(obj, comp, width) {
   }
 }
 
-function check(arr, arr2) {
+function DijkstraAction(array_stations, array_ways) {
 
-  for (let point of arr) {
+  for (let point of array_stations) {
 
-    for (let way of arr2) {
-      if (point.station == way.point_a && widthCompare(theStation(stations, way.point_b), way.distation, theStation(stations, way.point_a).weight) ||
-        (point.station == way.point_b && widthCompare(theStation(stations, way.point_a), way.distation, theStation(stations, way.point_b).weight))) {
+    for (let way of array_ways) {
+      if (point.station == way.point_a && weightCompare(theStation(array_stations, way.point_b), way.distation, theStation(array_stations, way.point_a).weight) ||
+        (point.station == way.point_b && weightCompare(theStation(array_stations, way.point_a), way.distation, theStation(array_stations, way.point_b).weight))) {
         return true;
       }
     }
@@ -128,19 +111,49 @@ function check(arr, arr2) {
 
 function shotestWay(start) {
 
-
   let begin;
-  let waysStorage = copymaker(railways);
-  let chk = true;
-  begin = stations[fireStarter(start)];
+  let pointCheck = true;
+  begin = theStation(stations, start.station);
   begin.weight = 0;
-  while (chk) {
-    chk = check(stations, waysStorage);
+  while (pointCheck) {
+    pointCheck = DijkstraAction(stations, railways);
   }
+
 }
 
-shotestWay(depo);
 
-for (let it of stations) {
-  it.showMeWeight()
+
+let domPoints = document.querySelectorAll('.stantion');
+var pip;
+var pop;
+var pap = true;
+pep=false;
+
+
+for (let domPoint of domPoints) {
+  domPoint.onclick = function() {
+
+    for (let point of stations) {
+
+      if(pep)
+      {for(let dom of domPoints){dom.style.background='white'; pep=false;}}
+
+      if (point.station == this.dataset.name && pap) {
+        pip = point;
+        console.log(pip);
+        shotestWay(pip);
+        pap = false;
+        domPoint.style.background='blue';
+        return null;
+
+      }
+      if (point.station == this.dataset.name && !pap) {
+        pop = point;
+        console.log(pop.weight);
+        domPoint.style.background='green';
+        pap = true;
+        pep=true;
+      }
+    }
+  }
 }
